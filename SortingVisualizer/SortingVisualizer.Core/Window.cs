f30492a036fd055e748bdc62f7683a7acfbfcf84
KerYagciHTL <kerimcan41@icloud.com>
2026-09-10 21:38:27 +0200

    feat: added Window.cs to create window

---
+++ SortingVisualizer/SortingVisualizer.Core/Window.cs
@@ -0,0 +1,37 @@
+﻿using Raylib_cs;
+
+namespace SortingVisualizer.Core;
+
+public class Window(WindowConfig config)
+{
+    public Color BackgroundColor { get; set; } = Color.Black;
+    
+    private bool _initialized;
+
+    public void Initialize()
+    {
+        Raylib.InitWindow(config.Width, config.Height, config.Title);
+        Raylib.SetTargetFPS(config.TargetFps);
+        _initialized = true;
+    }
+    
+    public void BeginFrame()
+    {
+        if (!_initialized) throw new InvalidOperationException("Window is not initialized");
+        
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(BackgroundColor);
+    }
+    
+    public void EndFrame()
+    {
+        if (!_initialized) throw new InvalidOperationException("Window is not initialized");
+        Raylib.EndDrawing();
+    }
+
+    public void Close()
+    {
+        if (!_initialized) throw new InvalidOperationException("Window is not initialized");
+        Raylib.CloseWindow();
+    }
+}
