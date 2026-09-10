8d465db34c69d39458623b5810d3dc015d13d01a
KerYagciHTL <kerimcan41@icloud.com>
2026-09-10 22:26:38 +0200

    feat: added the Visualizer.cs for visualization

---
+++ SortingVisualizer/SortingVisualizer.Core/Visualizer.cs
@@ -0,0 +1,39 @@
+﻿namespace SortingVisualizer.Core;
+
+public class Visualizer(Window window) : IDisposable
+{
+    private bool _initialized;
+    
+    public void Initialize()
+    {
+        window.Initialize();
+        _initialized = true;
+    }
+
+    public void Run()
+    {
+        if(!_initialized) throw new InvalidOperationException("Visualizer is not initialized");
+        try
+        {
+            Loop();
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.ToString());
+        }
+    }
+    
+    private void Loop()
+    {
+        while (!window.ShouldClose)
+        {
+            window.BeginFrame();
+            window.EndFrame();
+        }
+    }
+
+    public void Dispose()
+    {
+        if(_initialized) window.Close();
+    }
+}
