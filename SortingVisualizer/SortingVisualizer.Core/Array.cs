053b232f914a625ada29a843ce25fe717404fc75
KerYagciHTL <kerimcan41@icloud.com>
2026-09-11 12:42:59 +0200

    feat: added debuging text and points

--- SortingVisualizer/SortingVisualizer.Core/Array.cs
+++ SortingVisualizer/SortingVisualizer.Core/Array.cs
@@ -4,7 +4,9 @@ namespace SortingVisualizer.Core;
 
 public class Array(int[] array, WindowConfig config)
 {
+    public const int FontSize = 16;
     public const int Width = 150;
+    
     public void Draw()
     {
         var margin = (config.Width - array.Length * Width) / (array.Length + 1);
@@ -20,6 +22,23 @@ public class Array(int[] array, WindowConfig config)
             var barHeight = (int)(ratio * availableHeight);
 
             Raylib.DrawRectangle(curPosX, baseline - barHeight, Width, barHeight, Color.SkyBlue);
+    
+            if (config.IsDebugMode)
+            {
+                //Starting Points
+                Raylib.DrawCircle(curPosX, baseline, 5, Color.Red);
+                Raylib.DrawCircle(curPosX + Width, baseline, 5, Color.Red);
+                
+                //Display number
+                var text = value.ToString();
+                var textWidth = Raylib.MeasureText(text, FontSize);
+
+                var textX = curPosX + (Width - textWidth) / 2;
+                var textY = (baseline - barHeight) + (barHeight - FontSize) / 2;
+
+                Raylib.DrawText(text, textX, textY, FontSize, Color.Black);
+            }
+
             curPosX += Width + margin;
         }
     }
